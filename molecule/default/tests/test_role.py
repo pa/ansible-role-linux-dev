ba9533deb9fa61e23b3a7438428bb965f34049c8
pramodh.ayyappan92@gmail.com <pramodh.ayyappan92@gmail.com>
2021-11-29 16:52:26 +0530

    Updated fish config to only set fish as default shell

--- molecule/default/tests/test_role.py
+++ molecule/default/tests/test_role.py
@@ -1,7 +1,17 @@
 import testinfra
 
 def test_linux_dev(host):
-    assert host.file("/root/dev/resources/build-from-source").is_directory
-    assert host.file("/root/dev/resources/build-from-source/neovim/src").is_directory
-    assert host.package("myrepos").is_installed
+    # Check dev directories exists
+    assert host.file("/root/dev/archive").is_directory
+    assert host.file("/root/dev/config").is_directory
+    assert host.file("/root/dev/resources").is_directory
+    assert host.file("/root/dev/side").is_directory
+    assert host.file("/root/dev/working").is_directory
+    assert host.file("/root/dev/tmp").is_directory
+    
+    # Check all required packages installed
+    assert host.package("fish").is_installed
     assert host.package("tmux").is_installed
+    assert host.package("nvim").is_installed
+    assert host.package("fish").is_installed
+    assert host.package("myrepos").is_installed
